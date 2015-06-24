ee794e789c9137490293f322014f2ae3f03a3b6f
David Allsopp <david.allsopp@metastack.com>
2015-06-24 14:55:00 +0100

    Unix.getpid doesn't work as expected on Windows.
    Unix.getpid doesn't return the actual Windows Process ID (see
    http://caml.inria.fr/mantis/view.php?id=4034). What's worse is that it can
    return the same process ID for two separate running instances, which
    creates problems for OPAM's logging code which, reasonably, assumes unique
    process IDs.
    
    Patch calls the Windows API function GetCurrentProcessId.
    
    Code for the stubs is kept in src/core/stubs.c. This file should contain
    only functions in the form:
    
    CAMLprim value OPAMW_Name(value param1, value param2, ...)
    {
      CAMLparamn(value1, value2, ...);
    
      // Code here
    
      CAMLreturn(...);
    }
    
    This is used to generate "valid" (in a GC-sense) alternate stubs for
    non-Windows systems (although these should never be called).
    
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

---
+++ src/core/opamWindows.c
@@ -0,0 +1,31 @@
+/**************************************************************************/
+/*                                                                        */
+/*    Copyright 2015, 2016 MetaStack Solutions Ltd.                       */
+/*                                                                        */
+/*  This file is distributed under the terms of the GNU General Public    */
+/*  License version 3.0                                                   */
+/*                                                                        */
+/*  OPAM is distributed in the hope that it will be useful, but WITHOUT   */
+/*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    */
+/*  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public       */
+/*  License for more details.                                             */
+/*                                                                        */
+/**************************************************************************/
+
+#define CAML_NAME_SPACE
+#include <stdio.h>
+#include <caml/memory.h>
+#include <caml/fail.h>
+#include <caml/alloc.h>
+
+#ifdef WIN32
+
+#include <Windows.h>
+
+#include "stubs.c"
+
+#else
+
+#include "opamOther.c"
+
+#endif
