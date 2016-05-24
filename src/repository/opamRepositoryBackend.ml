618045d4e4c9c9ac253e4c4a75a6656cb8bf84c0
David Allsopp <david.allsopp@metastack.com>
2016-05-24 20:28:10 +0100

    Temporarily change default repository
    Port requires changes to the ocaml.system package in order for opam init
    succeed. Up to now, it was sufficient just to add an additional
    repository, but for now the default is changed to
    git+https://github.com/dra27/opam-repository#next-windows
    
    This commit must be removed before merge!

--- src/repository/opamRepositoryBackend.ml
+++ src/repository/opamRepositoryBackend.ml
@@ -37,9 +37,9 @@ let to_string r =
 let default_url = {
   OpamUrl.
   transport = "https";
-  path = "opam.ocaml.org";
-  hash = None;
-  backend = `http;
+  path = "github.com/dra27/opam-repository";
+  hash = Some "next-windows";
+  backend = `git;
 }
 
 let default () = {
