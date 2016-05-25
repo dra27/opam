6f70718bc57c0ab3dc1d0f7c5349fafdd12301d9
David Allsopp <david.allsopp@metastack.com>
2016-05-25 12:22:58 +0100

    Close all lock files on error
    Windows cannot erase directories if there are open file handles. Limited
    mechanism added for tracking lock handles to allow the .opam directory
    to be deleted if an error occurs during opam init.
    
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

--- src/core/opamSystem.ml
+++ src/core/opamSystem.ml
@@ -643,6 +643,12 @@ let string_of_lock_kind = function
   | `Lock_read -> "read"
   | `Lock_write -> "write"
 
+let locks = Hashtbl.create 16
+
+let release_all_locks () =
+  Hashtbl.iter (fun fd _ -> Unix.close fd) locks;
+  Hashtbl.clear locks
+
 let rec flock_update
   : 'a. ([< lock_flag ] as 'a) -> ?dontblock:bool -> lock -> unit
   = fun flag ?(dontblock=OpamCoreConfig.(!r.safe_mode)) lock ->
@@ -653,6 +659,7 @@ let rec flock_update
   else
   match flag, lock with
   | `Lock_none, { fd = Some fd; kind = (`Lock_read | `Lock_write); _ } ->
+    Hashtbl.remove locks fd;
     Unix.close fd; (* implies Unix.lockf fd Unix.F_ULOCK 0 *)
     lock.kind <- (flag :> lock_flag);
     lock.fd <- None
@@ -683,6 +690,7 @@ and flock: 'a. ([< lock_flag ] as 'a) -> ?dontblock:bool -> string -> lock =
   | flag ->
     mkdir (Filename.dirname file);
     let fd = Unix.openfile file Unix.([O_CREAT; O_RDWR]) 0o666 in
+    Hashtbl.add locks fd ();
     let lock = { fd = Some fd; file; kind = `Lock_none } in
     flock_update (flag :> lock_flag) ?dontblock lock;
     lock
