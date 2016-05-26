e69689b1b9c104c8b9dc2669445d1f536de1bcc2
David Allsopp <david.allsopp@metastack.com>
2016-05-26 21:22:11 +0100

    Improve variable interpolation of Windows paths
    Windows paths will routinely contain backslash characters, meaning that
    OPAM's variable interpolation (OpamFilter.expand_interpolations_in_file)
    will not work if the method is used to generate .config files.
    
    Alter the routine to probe the first line of the .in file and if it
    begins opam-version:, then escape variable expansions which occur within
    strings on each line.
    
    Thus, if the lib variable is C:\OPAM\system\lib then the following .in
    file:
    
    opam-version: "2.0~alpha"
    variables {
      lib: "%{lib}%/ocaml"
      broken: %{lib}%
    }
    
    expands to:
    
    opam-version: "2.0~alpha"
    variables {
      lib: "C:\\OPAM\\system\\lib/ocaml"
      broken: C:\OPAM\system\lib
    }
    
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

--- src/format/opamFilter.ml
+++ src/format/opamFilter.ml
@@ -73,6 +73,10 @@ let string_interp_regex =
       seq [str "%{"; group (greedy notclose); opt (group (str "}%"))];
     ])
 
+let escape_string =
+  let rex = Re.(compile @@ set "\\\"") in
+  Re_pcre.substitute ~rex ~subst:(fun s -> "\\"^s)
+
 let escape_expansions =
   let rex = Re.(compile @@ char '%') in
   Re_pcre.substitute ~rex ~subst:(fun s -> "%"^s)
@@ -185,7 +189,7 @@ let resolve_ident env fident =
     | None -> FString iffalse
 
 (* Resolves ["%{x}%"] string interpolations *)
-let expand_string ?default env text =
+let expand_string_aux ?(escape_string=fun x -> x) ?default env text =
   let subst str =
     if str = "%%" then "%"
     else if not (OpamStd.String.ends_with ~suffix:"}%" str) then
@@ -195,8 +199,11 @@ let expand_string ?default env text =
     let fident = String.sub str 2 (String.length str - 4) in
     resolve_ident env (filter_ident_of_string fident)
     |> value_string ?default:(OpamStd.Option.map (fun f -> f fident) default)
+    |> escape_string
   in
   Re_pcre.substitute ~rex:string_interp_regex ~subst text
+
+let expand_string = expand_string_aux ?escape_string:None
 
 let unclosed_expansions text =
   let re =
@@ -298,15 +305,38 @@ let expand_interpolations_in_file env file =
   let src = OpamFilename.add_extension f "in" in
   let ic = OpamFilename.open_in src in
   let oc = OpamFilename.open_out f in
-  let rec aux () =
-    match try Some (input_line ic) with End_of_file -> None with
+  let line = try Some (input_line ic) with End_of_file -> None in
+  let opamish =
+    match line with
+    | None ->
+        false
     | Some s ->
-      output_string oc (expand_string ~default:(fun _ -> "") env s);
+        match OpamStd.String.cut_at s ':' with
+          | Some (k, _) ->
+              if String.trim k = "opam-version" then
+                true
+              else
+                false
+          | None ->
+              false
+  in
+  let process (is_string, s) =
+    if is_string then
+      output_string oc (expand_string_aux ~escape_string ~default:(fun _ -> "") env s)
+    else
+      output_string oc (expand_string ~default:(fun _ -> "") env s)
+  in
+  let rec aux = function
+    | Some s ->
+      if opamish then
+        List.iter process (OpamInterpLexer.line (Lexing.from_string s))
+      else
+        process (false, s);
       output_char oc '\n';
-      aux ()
+      aux (try Some (input_line ic) with End_of_file -> None)
     | None -> ()
   in
-  aux ();
+  aux line;
   close_in ic;
   close_out oc
 
