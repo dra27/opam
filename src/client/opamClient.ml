b23fe21b08b5eeea0976d1827dee3faadb4c9bb9
David Allsopp <david.allsopp@metastack.com>
2016-06-05 08:39:55 +0100

    Add also-install field to opam file format
    also-install allows a filtered list of packages to be specified which
    should be installed with this package automatically when it first
    installed. The field is ignored on package upgrades and downgrades.
    
    Its purpose is to allow the flexlink package to be automatically in
    native Windows OCaml switches.
    
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

--- src/client/opamClient.ml
+++ src/client/opamClient.ml
@@ -274,7 +274,7 @@ let compute_upgrade_t
       (preprocessed_request t full_orphans orphan_versions
          ~wish_install:to_install
          ~wish_upgrade:(OpamSolution.atoms_of_packages to_upgrade)
-         ~criteria:`Upgrade ())
+         ~criteria:`Upgrade ()) |> fst
   else
   let changes =
     requested_installed ++ OpamSwitchState.packages_of_atoms t to_install
@@ -298,7 +298,7 @@ let compute_upgrade_t
        ~wish_install:to_install
        ~wish_remove:(OpamSolution.atoms_of_packages to_remove)
        ~wish_upgrade:upgrade_atoms
-       ())
+       ()) |> fst
 
 let upgrade_t ?strict_upgrade ?auto_install ?ask ?(check=false) ~all atoms t =
   log "UPGRADE %a"
@@ -419,7 +419,7 @@ let fixup t =
       (OpamSolver.request
          ~install:(OpamSolution.atoms_of_packages pkgs)
          ~criteria:`Fixup
-         ())
+         ()) |> fst
   in
   let is_success = function
     | _, Success _ -> true
@@ -912,7 +912,7 @@ let install_t t ?ask atoms add_to_roots ~deps_only =
     OpamSolution.resolve t Install
       ~orphans:(full_orphans ++ orphan_versions)
       ~requested:names
-      request in
+      request |> fst in
   let t, solution = match solution with
     | Conflicts cs ->
       log "conflict!";
@@ -1017,7 +1017,7 @@ let remove_t ?ask ~autoremove ~force atoms t =
              (OpamSolver.dependencies ~build:true
                 ~depopts:true ~installed:true universe to_remove))
       else to_remove in
-    let t, solution =
+    let t, solution, _ =
       OpamSolution.resolve_and_apply ?ask t Remove ~requested
         ~orphans:(full_orphans ++ orphan_versions)
         (OpamSolver.request
@@ -1072,7 +1072,7 @@ let reinstall_t t ?ask ?(force=false) atoms =
       ()
   in
 
-  let t, solution =
+  let t, solution, _ =
     OpamSolution.resolve_and_apply ?ask t Reinstall
       ~orphans:(full_orphans ++ orphan_versions)
       ~reinstall:(OpamPackage.packages_of_names t.installed requested)
