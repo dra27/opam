af4a08792ab0313c591bc225e258cbe63b14664b
David Allsopp <david.allsopp@metastack.com>
2021-03-20 10:50:25 +0000

    Bump the OPAMBSVERSION

--- .github/scripts/opam-bs-cache.sh
+++ .github/scripts/opam-bs-cache.sh
@@ -10,8 +10,19 @@ if [ "$os" = "darwin" ] ; then
   os=macos
 fi
 
-wget -q -O $OPAM_LOCAL/bin/opam-bootstrap \
-  "https://github.com/ocaml/opam/releases/download/$OPAMBSVERSION/opam-$OPAMBSVERSION-$(uname -m)-$os"
+pushd $OPAM_LOCAL/bin &> /dev/null
+git clone https://github.com/ocaml/opam.git
+cd opam
+./configure
+make lib-ext
+make
+cp opam $OPAM_LOCAL/bin/opam-bootstrap
+cd ..
+rm -rf opam
+popd &> /dev/null
+
+#wget -q -O $OPAM_LOCAL/bin/opam-bootstrap \
+#  "https://github.com/ocaml/opam/releases/download/$OPAMBSVERSION/opam-$OPAMBSVERSION-$(uname -m)-$os"
 cp -f $OPAM_LOCAL/bin/opam-bootstrap $OPAM_LOCAL/bin/opam
 chmod a+x $OPAM_LOCAL/bin/opam
 
