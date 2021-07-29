7ad35cd289abb959be211d196832185167721bf7
David Allsopp <david.allsopp@metastack.com>
2021-07-29 12:22:49 +0100

    fixup Cache the opam-repository clone commit

--- .github/scripts/archives-cache.sh
+++ .github/scripts/archives-cache.sh
@@ -8,4 +8,4 @@ which ocaml && export OCAML=`which ocaml` || true
 make -C src_ext cache-archives
 ls -al src_ext/archives
 rm -rf ~/opam-repository
-git clone https://github.com/ocaml/opam-repository.git ~/opam-repository --bare
+git clone $OPAM_REPO_MAIN ~/opam-repository --bare
