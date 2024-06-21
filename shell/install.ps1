492b64ac6115ba48db8e83eaf1b57d613ada449d
David Allsopp <david.allsopp@metastack.com>
2024-06-21 10:25:08 +0100

    Use a hashtable for the SHA512s

--- shell/install.ps1
+++ shell/install.ps1
@@ -24,31 +24,10 @@ $DevVersion = "2.2.0~rc1"
 $IsAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 $DefaultBinDir = If ($IsAdmin) {"$Env:ProgramFiles\opam\bin"} Else {"$Env:LOCALAPPDATA\Programs\opam\bin"}
 
-Function BinSHA512 {
-  param (
-    [string]$OpamBinName
-  )
-
-  switch ($OpamBinName) {
-    "opam-2.2.0-beta2-x86_64-windows.exe" { "74f034ccc30ef0b2041283ff125be2eab565d4019e79f946b515046c4c290a698266003445f38b91321a9ef931093651f861360906ff06c076c24d18657e2aaf" }
-    "opam-2.2.0-beta3-x86_64-windows.exe" { "f09337d94e06cedb379c5bf45a50a79cf2b2e529d7c2bb9b35c8a56d40902ff8c7e3f4de9c75fb5c8dd8272b87b2a2645b14e40ef965376ef0d19afd923acf3b" }
-    "opam-2.2.0-rc1-x86_64-windows.exe"   { "f2ec830a5706c45cb56a96713e296ef756c3f2904ca15f7c2ad0442916a9585fa1de8070208f2a6bb3a84dc74b677f946f5bc386c8ed1489da802b1d66a5e094" }
-    Default { throw "no sha" }
-  }
-}
-
-Function CheckSHA512 {
-  param (
-    [string]$OpamBinTmpLoc,
-    [string]$OpamBinName
-  )
-
-  $Hash = (Get-FileHash -Path $OpamBinTmpLoc -Algorithm SHA512).Hash
-  $HashTarget = BinSHA512 -OpamBinName "$OpamBinName"
-
-  if ("$Hash" -ne "$HashTarget") {
-    throw "Checksum mismatch, a problem occurred during download."
-  }
+$SHA512s = @{
+  "opam-2.2.0-beta2-x86_64-windows.exe" = "74f034ccc30ef0b2041283ff125be2eab565d4019e79f946b515046c4c290a698266003445f38b91321a9ef931093651f861360906ff06c076c24d18657e2aaf";
+  "opam-2.2.0-beta3-x86_64-windows.exe" = "f09337d94e06cedb379c5bf45a50a79cf2b2e529d7c2bb9b35c8a56d40902ff8c7e3f4de9c75fb5c8dd8272b87b2a2645b14e40ef965376ef0d19afd923acf3b";
+  "opam-2.2.0-rc1-x86_64-windows.exe" = "f2ec830a5706c45cb56a96713e296ef756c3f2904ca15f7c2ad0442916a9585fa1de8070208f2a6bb3a84dc74b677f946f5bc386c8ed1489da802b1d66a5e094";
 }
 
 Function DownloadAndCheck {
@@ -58,8 +37,16 @@ Function DownloadAndCheck {
     [string]$OpamBinName
   )
 
+  if (-not $SHA512s.ContainsKey($OpamBinName)) {
+    throw "no sha"
+  }
+
   Start-BitsTransfer -Source $OpamBinUrl -Destination $OpamBinTmpLoc -DisplayName "Downloading opam" -Description "Downloading $OpamBinName"
-  CheckSHA512 -OpamBinTmpLoc "$OpamBinTmpLoc" -OpamBinName "$OpamBinName"
+  $Hash = (Get-FileHash -Path $OpamBinTmpLoc -Algorithm SHA512).Hash
+
+  if ($Hash -ne "$($SHA512s[$OpamBinName])") {
+    throw "Checksum mismatch, a problem occurred during download."
+  }
 }
 
 if (-not [System.Environment]::Is64BitOperatingSystem) {
