0608ec5cb4e467479e0e9084b7ee3b631919cb8f
David Allsopp <david.allsopp@metastack.com>
2024-09-13 13:14:29 +0100

    Generate the stub names (reduce code duplication)

--- src/core/opamStubs.win32.pp.ml
+++ src/core/opamStubs.win32.pp.ml
@@ -1,3 +1,4 @@
+#2 "src/core/opamStubs.win32.pp.ml"
 (**************************************************************************)
 (*                                                                        *)
 (*    Copyright 2018 MetaStack Solutions Ltd.                             *)
@@ -12,6 +13,9 @@ include OpamStubsTypes
 include OpamWin32Stubs
 let getpid () = Int32.to_int (getCurrentProcessID ())
 
-external win_create_process : string -> string -> string option ->
-                              Unix.file_descr -> Unix.file_descr -> Unix.file_descr -> int
-                            = "win_create_process" "win_create_process_native"
+external win_create_process :
+  string -> string -> string option ->
+  Unix.file_descr -> Unix.file_descr -> Unix.file_descr -> int =
+(* The names of primitives are added by Dune (win_create_process /
+   win_create_process_native for OCaml 4.x and caml_unix_create_process /
+   caml_unix_create_process_native for OCaml 5.x *)
