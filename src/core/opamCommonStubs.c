914e52968eb8bf1588d1ac0ac9b0cc3399a5d68d
David Allsopp <david.allsopp@metastack.com>
2024-09-13 16:28:13 +0100

    Update header

--- src/core/opamCommonStubs.c
+++ src/core/opamCommonStubs.c
@@ -1,15 +1,10 @@
 /**************************************************************************/
 /*                                                                        */
-/*                                 OCaml                                  */
+/*    Copyright 2024 Kate Deplaix                                         */
 /*                                                                        */
-/*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           */
-/*                                                                        */
-/*   Copyright 1996 Institut National de Recherche en Informatique et     */
-/*     en Automatique.                                                    */
-/*                                                                        */
-/*   All rights reserved.  This file is distributed under the terms of    */
-/*   the GNU Lesser General Public License version 2.1, with the          */
-/*   special exception on linking described in the file LICENSE.          */
+/*  All rights reserved. This file is distributed under the terms of the  */
+/*  GNU Lesser General Public License version 2.1, with the special       */
+/*  exception on linking described in the file LICENSE.                   */
 /*                                                                        */
 /**************************************************************************/
 
